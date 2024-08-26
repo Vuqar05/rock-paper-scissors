ad9af745280c196cd785ed6d1227a954d8c1782c
VAhmadov <vahmadov23@ku.edu.tr>
2024-08-26 04:06:30 +0400

    Made a function to choose randomly rock paper or scissors

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello World!")
+
+function getComputerChoice() {
+    let randNum = Math.random()
+    if (randNum < .33) return "rock"
+    else if (randNum < .66 ) return "paper"
+    else return "scissors"
+}
+
+console.log(getComputerChoice())
