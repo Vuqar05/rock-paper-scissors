8fdc2ee1adb7a906087fd901352463a387b56746
VAhmadov <vahmadov23@ku.edu.tr>
2024-08-26 04:24:38 +0400

    Single round logic Added a function to check who wins the round, increment score accordingly and console log a string telling if the player won/lost or if it was a draw

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
     let randNum = Math.random()
     if (randNum < .33) return "rock"
@@ -9,3 +13,30 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt("Choose rock, paper or scissors")
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+
+    // Do nothing if equal
+    if (humanChoice === computerChoice) {
+        console.log("Draw")
+    }
+
+    // Human win scenarios
+    else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+             (humanChoice === 'scissors' && computerChoice === 'paper') ||
+             (humanChoice === 'paper' && computerChoice === 'rock')){
+        humanScore++
+        console.log(`You win because ${humanChoice} beats ${computerChoice}`)
+    }
+    // Computer wins
+    else {
+        computerScore ++
+        console.log(`You loose because ${computerChoice} beats ${humanChoice}`)
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
